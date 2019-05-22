f077890e65c3c76d379bc22a860824b7fbb8ed1a
Jen Johnson <jecajo87@gmail.com>
2019-05-22 09:04:09 -0500

    update index, style, and add js file

---
+++ script.js
@@ -0,0 +1,9 @@
+//scroll to project section
+$('.scroll').click(function() {
+   $.scrollTo($('.projects'));
+});
+
+//scroll to bio section
+$('.scroll').click(function() {
+   $.scrollTo($('.bio'));
+});
